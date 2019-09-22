7501f562ea2398933f9d8ef62590f03b28c48441
Nick B <nicholas.balestrieri@gmail.com>
2019-09-22 00:45:36 -0400

    adds a new game button that successfully shuffles the order of the cards

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const gameBox = document.getElementById("game-area");
 const matchNum = document.getElementById("match-num");
 const turnNum = document.getElementById("turn-num");
+const newGameButton = document.getElementById("new-game");
 
 // A deck of eight pairs of unicode characters
-const allCards = [
+const mainCards = [
   "\u2620",
   "\u2620",
   "\u2744",
@@ -22,16 +23,28 @@ const allCards = [
   "\u2714"
 ];
 
-// Make a deck of cards by adding the unicode characters in a random order
-
-let cardDeck;
+/*
+If there are nodes in the gameBox already, remove them.
+Make a deck of cards by adding the unicode characters in a random order.
+Add them all to the gameBox.
+*/
 
 function shuffle() {
-  cardDeck = [];
-  for (let i = 16; i; i--) {
+  while (gameBox.hasChildNodes()) {
+    gameBox.removeChild(gameBox.childNodes[0]);
+  }
+  const allCards = [...mainCards];
+  const cardDeck = [];
+  for (let i = 16; i > 0; i--) {
     const cardIdx = Math.floor(Math.random() * i);
     cardDeck.push(allCards.splice(cardIdx, 1));
   }
+  cardDeck.forEach(each => {
+    const div = document.createElement("DIV");
+    div.setAttribute("data-symbol", each);
+    div.addEventListener("click", clickHandler);
+    gameBox.appendChild(div);
+  });
 }
 
 shuffle();
@@ -42,6 +55,8 @@ more than two cards to display at a time.
 */
 
 let openTurn = false;
+
+let timer = false;
 
 /*
 The turnStatus object manages the state of the game.
@@ -64,21 +79,19 @@ const turnStatus = {
         openTurn = false;
         if (this.matches === 8) {
           alert("you win!");
-          this.matchesMade = [];
-          this.turnBack();
-          this.matches = 0;
-          this.turnsTaken = 0;
-          matchNum.innerHTML = this.matches;
-          turnNum.innerHTML = this.turnsTaken;
-          shuffle();
+          this.newGame();
           return;
         }
       } else {
         openTurn = true;
-        setTimeout(this.turnBack, 2200);
+        timer = setTimeout(this.turnBack, 2350);
       }
       this.turnsTaken += 1;
       turnNum.innerHTML = this.turnsTaken;
+      if (this.turnsTaken === 8) {
+        alert("you lose!");
+        this.newGame();
+      }
     }
   },
   turnBack: function() {
@@ -89,6 +102,16 @@ const turnStatus = {
       }
     });
     openTurn = false;
+  },
+  newGame: function() {
+    if (timer) clearTimeout(timer);
+    this.matchesMade = [];
+    this.turnBack();
+    this.matches = 0;
+    this.turnsTaken = 0;
+    matchNum.innerHTML = this.matches;
+    turnNum.innerHTML = this.turnsTaken;
+    shuffle();
   }
 };
 
@@ -101,12 +124,7 @@ function clickHandler(event) {
   }
 }
 
-cardDeck.forEach(each => {
-  const div = document.createElement("DIV");
-  div.setAttribute("data-symbol", each);
-  div.addEventListener("click", clickHandler);
-  gameBox.appendChild(div);
-});
+newGameButton.addEventListener("click", turnStatus.newGame.bind(turnStatus));
 
 matchNum.innerHTML = turnStatus.matches;
 turnNum.innerHTML = turnStatus.turnsTaken;
